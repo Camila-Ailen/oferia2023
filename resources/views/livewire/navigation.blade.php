231c2437f99b7d29b1adabf6ff0d1b6143222d77
Camila-Ailen <gcamilaailen@gmail.com>
2023-11-01 18:09:05 -0300

    haciendo el encabezado (todo a mano)

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,5 +1,15 @@
-<header>
-    <div class="container">
-        Hola MUNDOOO
+<header class="bg-gray-800">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center h-16">
+        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <span>Categorias</span>
+        </a>
+
+        <a href="/" class="mx-6">
+            <x-application-mark class="block h-9 w-auto" />
+        </a>
+        @livewire('search')
     </div>
 </header>
---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,3 @@
+<div class="flex-1">
+    <x-input type="text" class="w-full" placeholder="Estas buscando algun producto?" />
+</div>
