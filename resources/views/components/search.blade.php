db0362de32b474441158a868cba228aed8257b92
Camila-Ailen <gcamilaailen@gmail.com>
2023-11-02 16:37:52 -0300

    menu desplegable de user. Inclui fontawesome

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -16,6 +16,7 @@
     }
 @endphp
 
+{{-- ICONO DE BUSQUEDA  --}}
 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="{{$size}}" height="{{$size}}" viewBox="0,0,256,256">
     <g fill="{{$col}}" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
         <g transform="scale(2,2)">
